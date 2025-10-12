6c0793b79881b8665bb77855304d2f457075c3df
diart <diart@DESKTOP-GRU8VQP>
2025-10-12 23:43:06 +0200

    basic inheritance

--- src/Director.java
+++ src/Director.java
@@ -1,33 +1,11 @@
 import java.util.ArrayList;
 
-public class Director {
-    private String name;
-    private String surname;
-
+public class Director extends Person{
     private int numberOfShows;
-    private Gender gender;
 
-    public Director(String name, String surname, int numberOfShows, Gender gender) {
-        this.name = name;
-        this.surname = surname;
+    public Director(String name, String surname, int height, Gender gender, int numberOfShows) {
+        super(name,surname,height,gender);
         this.numberOfShows = numberOfShows;
-        this.gender = gender;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
     }
 
     public int getNumberOfShows() {
@@ -36,13 +14,5 @@ public class Director {
 
     public void setNumberOfShows(int numberOfShows) {
         this.numberOfShows = numberOfShows;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public void setGender(Gender gender) {
-        this.gender = gender;
     }
 }
